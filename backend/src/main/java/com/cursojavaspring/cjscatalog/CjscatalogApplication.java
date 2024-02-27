cb8c4d0cf4d37b75d6fb4477a6af29b4c535f200
wagner.augusto <wagner.augusto@tecnisys.com.br>
2024-02-27 16:41:05 -0300

    Backend project created

---
+++ backend/src/main/java/com/cursojavaspring/cjscatalog/CjscatalogApplication.java
@@ -0,0 +1,13 @@
+package com.cursojavaspring.cjscatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CjscatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CjscatalogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/cursojavaspring/cjscatalog/CjscatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursojavaspring.cjscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CjscatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
